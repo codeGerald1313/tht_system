ff3891e0acf5449a494ac79b9512e18204df07bd
GeraldCode <codegerald13@gmail.com>
2024-05-28 22:24:15 -0500

    MegaSuperGa

--- src/views/utility/invoice/grupo-edit.vue
+++ src/views/utility/invoice/grupo-edit.vue
@@ -755,8 +755,10 @@ export default {
     async fethById() {
       try {
         const response = await axios.get(`${import.meta.env.VITE_API_URL}/groups/record-formated/${this.idGrupo}`, {
-          params: { date: this.group.date_departure },  // Añadir el parámetro `date` a la solicitud
           ...headers,
+          params: {
+            date: this.group.date_departure
+          },
         });
         this.projects = response.data.data.group.bookings;
 
